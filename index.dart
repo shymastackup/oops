c97f0cae2066548aaa3a56f28ca7b4cc9bec0e64
shymastackup <shyma.stackup@gmail.com>
2024-08-16 15:52:47 +0530

    version

---
+++ index.dart
@@ -0,0 +1,11 @@
+class student{
+  String name;
+  int rollNo;
+  String email;
+  student(this.name,this.rollNo,this.email);
+
+}
+void main(){
+student stud1 = student("arun",4,"shyma@gmail.com");
+  print(stud1.email);
+}
